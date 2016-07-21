d9745379c5f8829c05bb177310aa7d7d08d97ba3
mohadipe <github@mohadipe.de>
2016-07-21 20:32:47 +0200

    Unter Android 6+ ist es nicht mehr ausreichend die benötigten Rechte im Manifest anzugeben. Nun muss auch danach gefragt werden zur Laufzeit. Davon abhängig wird der Button de/aktiviert.

---
+++ MobileMic/app/src/main/java/de/mohadipe/smartmic/FragmentInterface.java
@@ -0,0 +1,13 @@
+package de.mohadipe.smartmic;
+
+import android.view.View;
+
+public interface FragmentInterface {
+    void disableFunction();
+
+    void enableFunction();
+
+    void activateMic(View view);
+
+    void callPaypalMe();
+}
